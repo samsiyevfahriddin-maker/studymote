e793e4a78d04c9b7764b1196aea9c263d344e3e8
samsiyevfahriddin-maker <samsiyevfahriddin@gmail.com>
2026-08-23 21:06:08 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import asyncio
+import logging
+import sys
+
+from aiogram import Bot, Dispatcher
+from config import TOKEN
+from handlers import router
+
+
+async def main():
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher()
+
+    dp.include_router(router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        stream=sys.stdout
+    )
+
+    asyncio.run(main())
